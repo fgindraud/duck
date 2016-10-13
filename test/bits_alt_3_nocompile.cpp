a02903010bfc027ac09df0d542357d6693f83ecb
François Gindraud <francois.gindraud@gmail.com>
2016-10-13 23:05:10 +0200

    Cmake test with better nocompile mode
    Allow to compile-fail a file in multiple versions (through #defines),
    less files to manage...

---
+++ test/bits_alt_3_nocompile.cpp
@@ -0,0 +1,15 @@
+#include <cpp-utils/bits.h>
+struct A {
+	int a;
+};
+int main () {
+#if defined(TEST_CASE_1)
+	using B = Bits<int>;
+#elif defined(TEST_CASE_2)
+	using B = Bits<char>;
+#elif defined(TEST_CASE_3)
+	using B = Bits<A>;
+#endif
+	auto p = B::ones ();
+	return 0;
+}
