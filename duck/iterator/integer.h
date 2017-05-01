593d007e48c3b5f1749b50fb3f07811dd5d6dd9d
François Gindraud <francois.gindraud@gmail.com>
2017-05-01 16:13:02 +0200

    Integer iterator simplified (no facade use)

--- duck/iterator/integer.h
+++ duck/iterator/integer.h
@@ -3,37 +3,61 @@
 // Defines IntegerIterator (iterates on integral number spaces).
 // Allows to generate an integer range with all range features.
 
-#include <duck/iterator/facade.h>
+#include <iterator>
 
 namespace duck {
 
 namespace Iterator {
-	namespace Detail {
-		template <typename Int> class IntegerImpl {
-		public:
-			using iterator_category = std::random_access_iterator_tag;
-			using value_type = Int;
-			using difference_type = std::ptrdiff_t;
-			using pointer = const value_type *;
-			using reference = const value_type &;
+	template <typename Int> class Integer {
+	public:
+		using iterator_category = std::random_access_iterator_tag;
+		using value_type = Int;
+		using difference_type = std::ptrdiff_t;
+		using pointer = const value_type *;
+		using reference = value_type; // Not standard compliant
 
-		protected:
-			IntegerImpl () noexcept = default;
-			IntegerImpl (Int n) noexcept : n_ (n) {}
-			// + default ctors
+		constexpr Integer () noexcept = default;
+		constexpr Integer (Int n) noexcept : n_{n} {}
 
-			reference deref () const { return n_; }
-			void next () { ++n_; }
-			void prev () { --n_; }
-			void advance (difference_type n) { n_ += n; }
-			bool equal (const IntegerImpl & it) const { return distance (it) == 0; }
-			difference_type distance (const IntegerImpl & it) const { return n_ - it.n_; }
+		// Input / output
+		Integer & operator++ () noexcept { return ++n_, *this; }
+		constexpr reference operator* () const noexcept { return n_; }
+		constexpr pointer operator-> () const noexcept { return &n_; }
+		constexpr bool operator== (const Integer & o) const noexcept { return n_ == o.n_; }
+		constexpr bool operator!= (const Integer & o) const noexcept { return n_ != o.n_; }
 
-		private:
-			Int n_{};
-		};
-	}
+		// Forward
+		Integer operator++ (int) noexcept {
+			Integer tmp (*this);
+			++*this;
+			return tmp;
+		}
 
-	template <typename Int> using Integer = Facade<Detail::IntegerImpl<Int>>;
+		// Bidir
+		Integer & operator-- () noexcept { return --n_, *this; }
+		Integer operator-- (int) noexcept {
+			Integer tmp (*this);
+			--*this;
+			return tmp;
+		}
+
+		// Random access
+		Integer & operator+= (difference_type n) noexcept { return n_ += n, *this; }
+		constexpr Integer operator+ (difference_type n) const noexcept { return Integer (n_ + n); }
+		friend constexpr Integer operator+ (difference_type n, const Integer & it) noexcept {
+			return it + n;
+		}
+		Integer & operator-= (difference_type n) noexcept { return n_ -= n, *this; }
+		constexpr Integer operator- (difference_type n) const noexcept { return Integer (n_ - n); }
+		constexpr difference_type operator- (const Integer & o) const noexcept { return n_ - o.n_; }
+		constexpr reference operator[] (difference_type n) const noexcept { return n_ + n; }
+		constexpr bool operator< (const Integer & o) const noexcept { return n_ < o.n_; }
+		constexpr bool operator> (const Integer & o) const noexcept { return n_ > o.n_; }
+		constexpr bool operator<= (const Integer & o) const noexcept { return n_ <= o.n_; }
+		constexpr bool operator>= (const Integer & o) const noexcept { return n_ >= o.n_; }
+
+	private:
+		Int n_{};
+	};
 }
 }
