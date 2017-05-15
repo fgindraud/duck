97018a1d911111751a6bf93c16eb106f6ebcbca4
François Gindraud <francois.gindraud@univ-lyon1.fr>
2017-05-15 17:33:46 +0200

    Added demangling support

--- duck/debug.h
+++ duck/debug.h
@@ -4,15 +4,38 @@
 
 #include <iostream>
 
+#ifdef DUCK_HAVE_DEMANGLING
+#include <cstdlib>
+#include <cxxabi.h>
+#include <memory>
+#endif
+
 namespace duck {
+
+inline std::string demangle (const char * name) {
+#ifdef DUCK_HAVE_DEMANGLING
+	int status{};
+	std::unique_ptr<char, void (*) (void *)> res{
+	    abi::__cxa_demangle (name, nullptr, nullptr, &status), std::free};
+	return status == 0 ? res.get () : name;
+#else
+	return name;
+#endif
+}
 
 class Noisy {
 public:
 	Noisy () { msg ("default constr"); }
 	Noisy (const Noisy &) { msg ("copy constr"); }
 	Noisy (Noisy &&) { msg ("move constr"); }
-	Noisy & operator= (const Noisy &) { msg ("copy assign"); }
-	Noisy & operator= (Noisy &&) { msg ("move assign"); }
+	Noisy & operator= (const Noisy &) {
+		msg ("copy assign");
+		return *this;
+	}
+	Noisy & operator= (Noisy &&) {
+		msg ("move assign");
+		return *this;
+	}
 	~Noisy () { msg ("destr"); }
 
 private:
