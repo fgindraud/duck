36b7eea5d94551a39952d854b60e294fdce78daf
François Gindraud <francois.gindraud@gmail.com>
2018-04-07 17:50:08 +0200

    Added prototype for Ref<..>

--- duck/dynamic_struct.h
+++ duck/dynamic_struct.h
@@ -7,11 +7,26 @@ namespace duck {
 
 using Integer = std::ptrdiff_t;
 
+// Pointers
+struct RefBase {
+	void * p;
+	void * get () const noexcept { return p; }
+};
+template <typename T> struct Ref : RefBase {
+	T * get () const noexcept { return static_cast<T *> (p); }
+	T & operator* () const noexcept { return *get (); }
+	T * operator-> () const noexcept { return get (); }
+};
+
+// Struct<...> & related defs
 template <typename... Elements> struct Struct;
 
 template <typename T> struct IsStruct : std::false_type {};
 template <typename... Elements> struct IsStruct<Struct<Elements...>> : std::true_type {};
 
+template <typename... Elements> struct Ref<Struct<Elements...>> : RefBase {};
+
+// TypeInfo
 template <typename T> struct TypeInfo {
 	static_assert (!IsStruct<T>::value, "TypeInfo must be specialised for Struct<Elements...>");
 
